4e0be8fdd86d215e235a7958a8b7688cc92ebf86
Nish <ndszz306@gmail.com>
2026-01-07 22:42:17 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  // 👇 User-site repo = must be "/"
+  base: "/",
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
+
   server: {
     host: true,
     allowedHosts: [
-      'propagatory-ernestina-sightliest.ngrok-free.dev'
-    ]
-  }
-})
+      "propagatory-ernestina-sightliest.ngrok-free.dev",
+    ],
+  },
+});
